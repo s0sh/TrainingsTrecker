7175551b9a5a8a9d45be7718c4ef59652049f708
r.bigun@devlight.io <r.bigun@devlight.io>
2023-04-05 13:08:38 +0300

    - Refactoring - Added white dot to timer

--- WorkoutApp/Helpers/Resources/Resources.swift
+++ WorkoutApp/Helpers/Resources/Resources.swift
@@ -12,12 +12,12 @@ typealias R = Resources
 enum Resources {
    
     enum Colors {
-        static var active = UIColor(hexString: "#437BFE")
-        static var inactive = UIColor(hexString: "#929DA5")
-        static var separator = UIColor(hexString: "#E8ECEF")
-        static var titleDarkGrey = UIColor(hexString: "#545C77")
-        static var backgroundMain = UIColor(hexString: "#F8f9F9")
-        static var secondaryBackground = UIColor(hexString: "#F0F3FF")
+        static let active = UIColor(hexString: "#437BFE")
+        static let inactive = UIColor(hexString: "#929DA5")
+        static let separator = UIColor(hexString: "#E8ECEF")
+        static let titleDarkGrey = UIColor(hexString: "#545C77")
+        static let backgroundMain = UIColor(hexString: "#F8f9F9")
+        static let secondaryBackground = UIColor(hexString: "#F0F3FF")
     }
     
     enum Strings {
@@ -34,21 +34,21 @@ enum Resources {
         
         enum NavBar {
             enum Title {
-                static var overview = "Today"
-                static var session = "Hight Intensity Cardio"
-                static var progress = "Workout Progress"
-                static var settings = "Settings"
+                static let overview = "Today"
+                static let session = "Hight Intensity Cardio"
+                static let progress = "Workout Progress"
+                static let settings = "Settings"
             }
         }
         
         enum SessionButton {
-            static var navBarLeft = "Pause"
-            static var navBarRight = "Finish"
+            static let navBarLeft = "Pause"
+            static let navBarRight = "Finish"
         }
         
         enum ProgressButton {
-            static var navBarLeft = "Export"
-            static var navBarRight = "Details"
+            static let navBarLeft = "Export"
+            static let navBarRight = "Details"
         }
         
         enum Overview {
